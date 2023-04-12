d4aa92fa07962de15a7a8c96e64daa9ee47a8bd5
Mohammed Hatim <hatim.developer@gmail.com>
2023-04-12 17:57:28 +0530

    A platform, player, wood stick, water and play around with editor

---
+++ Assets/PrintToConsole.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrintToConsole : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Ate...too...many...donuts!");
+    }
+}
